1cd32744f6f8bdb81a42dc77a32112874f94e230
Grete Ojavere <GreteO@users.noreply.github.com>
2017-09-11 14:42:41 +0300

    Lisatud esimene PHP osa

--- Esimene.php
+++ Esimene.php
@@ -9,6 +9,14 @@
 	<p>Tere!<p/>
 	<p>Olete jõudnud isiklikule veebilehele.<p/>
 	<p>See leht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.<p/>
+	<?php
+		echo "<p>Kõige esimene PHP abil väljastatud sõnum.</p>";
+		echo "<p>Täna on ";
+		echo date("d.m.Y");
+		echo ".<p/>";
+		echo "<p>Lehe avamise hetkel oli kell " .date("H:i:s") .".<p/>";
+
+ 	?>
 
 </body>
 </html>
