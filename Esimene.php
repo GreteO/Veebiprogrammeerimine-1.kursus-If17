473ea0f1ddbb140373e7023c72a3f7316fed5e7e
Grete Ojavere <GreteO@users.noreply.github.com>
2017-09-11 13:49:10 +0300

    Esimene PHP fail, kus on vaid HTML

---
+++ Esimene.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Grete Ojavere veebiprogrammeerimine</title>
+</head>
+<body>
+	<h1>Grete Ojavere</h1>
+	<p>Tere!<p/>
+	<p>Olete jõudnud isiklikule veebilehele.<p/>
+	<p>See leht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.<p/>
+
+</body>
+</html>
